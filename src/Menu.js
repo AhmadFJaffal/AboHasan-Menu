857d1179f703d48729382be4b22929b8df190105
unknown <jaffal250@gmail.com>
2024-10-30 18:53:48 +0200

    Update Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -7,18 +7,31 @@ const Menu = ({ items }) => {
     let priceDisplay = []; // To hold formatted price strings
 
     // Determine if the 'Per Meal' suffix should be used
-    const perMealSuffix = item.kiloPrice && item.kiloPrice !== "NaN" ? " (Per Meal)" : "";
+    const perMealSuffix =
+      item.kiloPrice && item.kiloPrice !== "NaN" ? " (Per Meal)" : "";
 
     // Check and format the meal price (regular price)
     if (!item.price || item.price === "NaN") {
-      priceDisplay.push(<h4 className="price" key="meal-price">X LBP</h4>); // Default display for invalid or missing prices
+      priceDisplay.push(
+        <h4 className="price" key="meal-price">
+          X LBP
+        </h4>
+      ); // Default display for invalid or missing prices
     } else {
-      priceDisplay.push(<h4 className="price" key="meal-price">{item.price} LBP{perMealSuffix}</h4>);
+      priceDisplay.push(
+        <h4 className="price" key="meal-price">
+          {item.price} LBP{perMealSuffix}
+        </h4>
+      );
     }
 
     // Check and format the kilo price if it exists and is not "NaN"
     if (item.kiloPrice && item.kiloPrice !== "NaN") {
-      priceDisplay.push(<h4 className="price" key="kilo-price">{item.kiloPrice} LBP (Per Kilo)</h4>);
+      priceDisplay.push(
+        <h4 className="price" key="kilo-price">
+          {item.kiloPrice} LBP (Per Kilo)
+        </h4>
+      );
     }
 
     return priceDisplay; // Return the array of JSX elements
@@ -26,18 +39,20 @@ const Menu = ({ items }) => {
 
   return (
     <div className="section-center">
-      {items.map((menuItem) => (
-        <article key={menuItem.id} className="menu-item">
-          <div className="item-info">
-            <header>
-              <h4>{menuItem.title}</h4>
-              {/* Display formatted prices */}
-              {formatPrice(menuItem)}
-            </header>
-            <p className="item-text">{menuItem.desc}</p>
-          </div>
-        </article>
-      ))}
+      {items
+        .filter((menuItem) => menuItem.title)
+        .map((menuItem) => (
+          <article key={menuItem.id} className="menu-item">
+            <div className="item-info">
+              <header>
+                <h4>{menuItem.title}</h4>
+                {/* Display formatted prices */}
+                {formatPrice(menuItem)}
+              </header>
+              <p className="item-text">{menuItem.desc}</p>
+            </div>
+          </article>
+        ))}
     </div>
   );
 };
